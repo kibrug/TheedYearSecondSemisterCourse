d3f0bc9592045d4612f7737e9a5e3c12fda1ef3c
Kibru Gebremedhin <87245699+kibrug@users.noreply.github.com>
2022-12-28 23:31:54 +0300

    Networking  project added
    More about networking in java TCP and UDP by applying Server and client programing

---
+++ NatiworkingProject/src/Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main {
+
+
+
+    public static void main(String[] args) throws Exception {
+
+  try {
+      System.out.println("Networking Projects In Java");
+  }
+  catch (Exception e){
+      System.out.println(e);
+  }
+
+
+    }
+}
